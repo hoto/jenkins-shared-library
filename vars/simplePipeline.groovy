b80d1b3ab131580ba51c94d3d97dd65530ec2a66
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-10-27 20:08:39 +0100

    Add build number to build title in simplePipeline

--- vars/simplePipeline.groovy
+++ vars/simplePipeline.groovy
@@ -5,7 +5,7 @@ def call(Map args) {
   node {
     def build = args.jsl.com.mycompany.jenkins.Build.new(this)
     build.setBuildDescription([
-      title      : 'My build title',
+      title      : "#${env.BUILD_NUMBER} My build title",
       description: 'My build description'
     ])
   }
